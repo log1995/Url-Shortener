bb0ae530ccbf0382cdce7b5a84d8e3dd7ab6bd31
이원기 <log19951@gmail.com>
2022-07-23 21:01:28 +0900

    test code 추가

--- src/test/java/com/log1995/urlshortener/application/UrlShortenerServiceIntegrationTest.java
+++ src/test/java/com/log1995/urlshortener/application/UrlShortenerServiceIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.log1995.urlshortener.application;
 
 import com.log1995.urlshortener.domain.UrlShortenerRepository;
-import com.log1995.urlshortener.domain.User;
 import com.log1995.urlshortener.presentation.UserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @Profile("prod")
 @SpringBootTest
@@ -31,6 +31,18 @@ class UrlShortenerServiceIntegrationTest {
 
         String originUrl = urlShortenerService.findOriginUrl(userDto.getChangedUrl());
         assertThat(userDto.getOriginUrl()).isEqualTo(originUrl);
+    }
+
+    @Test
+    void 단축URL을_조회하면_조회_횟수가_1씩_증가한다() {
+        UserDto userDto = new UserDto();
+        userDto.setOriginUrl("https://www.naver.com");
+
+        urlShortenerService.changeUrl(userDto);
+        urlShortenerService.findOriginUrl(userDto.getChangedUrl());
+
+        int responseCount = urlShortenerService.findResponseCount("localhost:8080/" + userDto.getChangedUrl());
+        assertTrue(responseCount == 1);
     }
 
 }
--- src/test/java/com/log1995/urlshortener/application/UrlShortenerServiceTest.java
+++ src/test/java/com/log1995/urlshortener/application/UrlShortenerServiceTest.java
@@ -3,17 +3,15 @@ package com.log1995.urlshortener.application;
 import com.log1995.urlshortener.domain.UrlShortenerRepository;
 
 import com.log1995.urlshortener.domain.User;
-import com.log1995.urlshortener.infrastructure.DataBaseUrlShortenerRepository;
-import com.log1995.urlshortener.infrastructure.MemoryUrlShortenerRepository;
 import com.log1995.urlshortener.presentation.UserDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.OngoingStubbing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -43,24 +41,31 @@ public class UrlShortenerServiceTest {
          assertThat(userDto.getOriginUrl()).isEqualTo(originUrl);
     }
 
-//    public User(String originUrl, String changedUrl, int responseTime) {
-//        this.originUrl = originUrl;
-//        this.changedUrl = changedUrl;
-//        this.responseTime = responseTime;
-//    }
+    @Test
+    public void count() {
+        // Given
+        String ORIGIN_URL = "https://www.naver.com";
+        int RESPONSE_COUNT = 1;
+
+        UserDto userDto = new UserDto();
+        userDto.setResponseTime(RESPONSE_COUNT);
+
+        when(urlShortenerRepository.findResponseCountInUser(any())).thenReturn(new User(ORIGIN_URL, "1q2w3e4r", 1));
+        int responseTime = urlShortenerService.findResponseCount("ddd/ddd");
+
+        assertTrue(userDto.getResponseTime() == responseTime);
+    }
 
 //    @Test
-//    void 원본URL을_단축하고_조회하면_원본URL이_조회된다() {
+//    void 단축URL을_조회하면_조회_횟수가_1씩_증가한다() {
 //        UserDto userDto = new UserDto();
 //        userDto.setOriginUrl("https://www.naver.com");
 //
 //        urlShortenerService.changeUrl(userDto);
-//        // 변경 + 저장
+//        urlShortenerService.findOriginUrl(userDto.getChangedUrl());
 //
-////        String changedUrl = urlShortenerService.findChangedUrl(userDto.getOriginUrl());
-//
-//        String originUrl = urlShortenerService.findOriginUrl(userDto.getChangedUrl());
-//        assertThat(userDto.getOriginUrl()).isEqualTo(originUrl);
+//        int responseCount = urlShortenerService.findResponseCount("localhost:8080/" + userDto.getChangedUrl());
+//        assertThat(userDto.getResponseTime() + 10000 == responseCount);
 //    }
 
 }
